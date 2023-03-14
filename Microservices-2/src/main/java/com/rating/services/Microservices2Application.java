c16aa02ee7821549498acb8c6f28febac78ddecb
Ayush gupta <ayush111195>
2023-03-15 00:28:07 +0530

    CONNECTED WITH EUREKA

--- Microservices-2/src/main/java/com/rating/services/Microservices2Application.java
+++ Microservices-2/src/main/java/com/rating/services/Microservices2Application.java
@@ -2,8 +2,10 @@ package com.rating.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Microservices2Application {
 
 	public static void main(String[] args) {
